ebcde05872d864bee5ba070ddfbffd25a49bab98
Mathias Calle <mathiascalle@Mathiass-MacBook-Air.local>
2025-06-05 09:04:47 -0500

    Fix: importar index.css para aplicar estilos de Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'  // <-- esto es nuevo, para que cargue Tailwind y tus estilos
+
 import App from './App'
 import { createClient } from '@supabase/supabase-js'
 import { SupabaseProvider } from './supabaseContext'
 
 const supabaseUrl = 'https://rpoveykqvpinreccivhe.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwb3ZleWtxdnBpbnJlY2NpdmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMjUwNDYsImV4cCI6MjA2NDcwMTA0Nn0.kZAF5Ges1DyJeVakr_lhGxyQsGBiOR4c0_eW84NpxeQ'
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -15,5 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </SupabaseProvider>
   </React.StrictMode>
 )
-
 
